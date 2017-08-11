32843b67a7b19cefef71f54dbd78eefebd1915b7
Kelauni22 <Kelauni.Cook@gmail.com>
2017-08-11 10:38:45 -0400

    initial classes made

--- blog.rb
+++ blog.rb
@@ -1,1 +1,31 @@
-test
+#Each time a new post is created, it will be an instance of the Post object
+
+
+#Author has a name, a password, and a series of posts (own class).
+class Author
+  def initialize(name, password, post)
+    @name = name
+    @password = password
+    @post = post
+  end
+end
+
+#Post has title (string), author (string), body (string), collection of labels (array),
+#and comments (own class)
+class Post
+  def initialize(title, author, body, labels, comments)
+    @title = title
+    @author = author
+    @body = body
+    @labels = labels
+    @comments = comments
+end
+
+#Comments have an author (string) and a comment (string)
+class Comments
+  def initialize(author, comment)
+    @author = author
+    @comment = comment
+end
+
+#Ask the user for a name and password
