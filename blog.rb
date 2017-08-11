93bd36749b4d524c9f1908c68d7ecf1e14cb498f
Kelauni22 <Kelauni.Cook@gmail.com>
2017-08-11 12:41:23 -0400

    create blog_classes.rb file with classes

--- blog.rb
+++ blog.rb
@@ -1,31 +1,4 @@
-#Each time a new post is created, it will be an instance of the Post object
-
-
-#Author has a name and a series of posts (own class).
-class Author
-  def initialize(name, post)
-    @name = name
-    @post = post
-  end
-end
-
-#Post has title (string), author (string), body (string), collection of labels (array),
-#and comments (own class)
-class Post
-  def initialize(title, author, body, labels, comments)
-    @title = title
-    @author = author
-    @body = body
-    @labels = labels
-    @comments = comments
-end
-
-#Comments have an author (string) and a comment (string)
-class Comments
-  def initialize(author, comment)
-    @author = author
-    @comment = comment
-end
+posts = {}
 
 #Give user choices
 puts"What would you like to do?"
@@ -35,8 +8,18 @@ puts "[3] Write a Blog"
 puts "[4] Update a Blog"
 puts "[5] Delete a Blog"
 
+#get the choice from the user
 choice = gets.chomp
-choice.downcase!
 
+#code for each choice in case statement
 case choice
-when
+  when "1"
+    puts posts
+    #This must display the title, author, post, labels, and comments
+  when "2"
+  when "3"
+  when "4"
+  when "5"
+  else
+    puts "Your input is not an option."
+    #reloop here
---
+++ blog_classes.rb
@@ -0,0 +1,30 @@
+#Each time a new post is created, it will be an instance of the Post object
+
+
+#Author has a name and a series of posts (own class).
+class Author
+  def initialize(name, post)
+    @name = name
+    @post = post
+  end
+end
+
+#Post has title (string), author (string), body (string), collection of labels (array),
+#and comments (own class)
+class Post
+  def initialize(title, author, body, labels, comments)
+    @title = title
+    @author = author
+    @body = body
+    @labels = labels
+    @comments = comments
+  end
+end
+
+#Comments have an author (string) and a comment (string)
+class Comments
+  def initialize(author, comment)
+    @author = author
+    @comment = comment
+  end
+end
